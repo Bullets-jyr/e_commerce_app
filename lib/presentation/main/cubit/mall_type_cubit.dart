98dfaff198281804d463d52788017d5216ca7a18
Bullets-jyr <jyr@bullets.co.kr>
2024-01-04 09:17:18 +0900

    06. 메인 화면 구조 잡기 - App Bar UI 개발

--- lib/presentation/main/cubit/mall_type_cubit.dart
+++ lib/presentation/main/cubit/mall_type_cubit.dart
@@ -1,5 +1,7 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../core/theme/custom/custom_app_bar.dart';
+
 enum MallType {
   market,
   beauty
@@ -15,9 +17,18 @@ extension MallTypeX on MallType {
   String get toName {
     switch (this) {
       case MallType.market:
-        return '마켓';
+        return '마켓블렛츠';
       case MallType.beauty:
-        return '뷰티';
+        return '뷰티블렛츠';
+    }
+  }
+
+  CustomAppBarTheme get theme {
+    switch (this) {
+      case MallType.market:
+        return CustomAppBarTheme.market;
+      case MallType.beauty:
+        return CustomAppBarTheme.beauty;
     }
   }
 
